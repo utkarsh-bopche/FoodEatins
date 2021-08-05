b02a66c8f54e822e5e671a37712b9a0336d575f5
Utkarsh Bopche <utkarsh.bopche@thinkitive.com>
2021-08-05 17:56:45 +0530

    Initial Commit

---
+++ FoodEatins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodEatins
+//
+//  Created by Apple on 05/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
